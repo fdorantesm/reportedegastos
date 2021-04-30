e8e4eba5d6b0c33b7efdff7d465b7534bfbd1e3a
Fernando Dorantes <fernando@dorant.es>
2021-04-29 23:18:19 -0500

    Initial commit: 🚀 Nice trip

---
+++ precache-manifest.f7ac4332c540210acaccd0cec374850e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd51e8582b18abee619",
+    "url": "/css/app.f9017bc7.css"
+  },
+  {
+    "revision": "3ef2fd24efc6003bf438",
+    "url": "/css/chunk-vendors.eff9c8f2.css"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc4.28546717.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxK.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "76f934f20544e6ec7c9ec4caf86d8caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dd51e8582b18abee619",
+    "url": "/js/app.7cbee85e.js"
+  },
+  {
+    "revision": "3ef2fd24efc6003bf438",
+    "url": "/js/chunk-vendors.11b634a2.js"
+  },
+  {
+    "revision": "cbdcc92b2fd991759473",
+    "url": "/js/perfil.465ede43.js"
+  },
+  {
+    "revision": "f7265f23fe820752a642",
+    "url": "/js/reportes.66345f34.js"
+  },
+  {
+    "revision": "c9080cf9509983cf3cc2f2cc0ffcf720",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
